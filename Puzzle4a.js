b689456d9115809614effaaf06c11e07b5720cdb
kendrawong <kendra.m.wong@gmail.com>
2018-01-29 00:48:24 -0800

    Create Puzzle4a.js

---
+++ Puzzle4a.js
@@ -0,0 +1,25 @@
+//mondrian
+//border 10
+//space between rectangles 5
+
+background(0, 0, 0);
+
+fill("white");
+rect(10, 115, 60, 120);
+rect(75, 240, 120, 60);
+rect(200, 240, 55, 25);
+rect(200, 10, 55, 40);
+
+fill("blue");
+rect(260, 305, 70, 25);
+rect(75, 145, 180, 90);
+rect(75, 10, 120, 40);
+
+fill("red");
+rect(10, 10, 60, 100);
+rect(10, 280, 60, 50);
+rect(200, 270, 55, 30);
+
+fill("yellow");
+rect(260, 10, 70, 255);
+rect(75, 305, 180, 25);
