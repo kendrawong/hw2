768d3002bc8936e98a320de472fb7c6b47f32c5a
kendrawong <kendra.m.wong@gmail.com>
2018-01-31 08:34:29 -0800

    Update Puzzle3a.js

--- Puzzle3a.js
+++ Puzzle3a.js
@@ -4,6 +4,6 @@ noStroke();
 
 while(true) {
   background(0, 0, 255, 0.1);
-  fill(random(40), 110, 90);
+  fill(random(40, 60), 110, 90);
   ellipse(random(width), random(height), random(20, 40));
 }
